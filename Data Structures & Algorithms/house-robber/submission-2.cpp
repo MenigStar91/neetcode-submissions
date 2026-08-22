350f1aa54a03d261841a88fcb44b5377a1f5cde6
Diksha Jena <76737246+MenigStar91@users.noreply.github.com>
2026-08-22 16:37:04 +0530

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        int x = 0;
+        int y = nums[0];
+        for(int i=1;i<n;i++){
+            int t = max(x+nums[i], y);
+            x = y;
+            y = t;
+        }
+        return y;
+    }
+};
